5f6f6c5185e324589df66a549d773020efe8835a
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-01 10:09:56 +0100

    Add router component

---
+++ src/App/components/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "../App";
+
+const Router = () => (
+  <BrowserRouter basename={"/"}>
+    <Switch>
+      <Route exact={true} path="/" component={App} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
