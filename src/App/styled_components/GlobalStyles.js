4b57efcb5f44906ca54f5b2589921f82c5d5b1ce
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-04 12:53:52 +0100

    :lipstick: Add global styles

---
+++ src/App/styled_components/GlobalStyles.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components'
+import styledNormalize from 'styled-normalize'
+
+const GlobalStyle = createGlobalStyle`
+  
+  ${styledNormalize}
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+  }
+
+  p {
+    line-height: 1.8;
+    font-size: 1.1rem;
+  }
+`
+
+export default GlobalStyle
