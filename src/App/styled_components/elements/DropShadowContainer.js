2e3e1b7102241ec45de85aa05eb9fe4a8bd8296d
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-05 23:35:31 +0100

    Modify styling on DropShadowContainer

--- src/App/styled_components/elements/DropShadowContainer.js
+++ src/App/styled_components/elements/DropShadowContainer.js
@@ -2,11 +2,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const DropShadowContainer = styled.div`
-  background-color: #fff;
+  background-color: #f8f9fa;
   box-shadow: rgba(25, 4, 69, 0.1) 0px 4px 10px;
   padding: 20px 50px;
   border-radius: 5px;
-  margin-bottom: 50px;
+  margin: 50px 0;
 `
 
 export default DropShadowContainer
