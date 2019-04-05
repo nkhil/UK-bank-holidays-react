163f99a4dc5e29673268e84b23028336b3218a8f
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-05 22:52:46 +0100

    Add styles to Header component

--- src/App/components/Header.js
+++ src/App/components/Header.js
@@ -1,17 +1,22 @@
 import React, { PureComponent, Fragment } from 'react'
+import Heading from '../styled_components/elements/Heading'
+import P from '../styled_components/elements/P'
+import DropShadowContainer from '../styled_components/elements/DropShadowContainer'
 
 class Header extends PureComponent {
   render() {
     return (
       <Fragment>
-        <p>The next bank holiday is in</p>
-        <h1>
-          {this.props.futureHolidays.length !== 0
-            ? `${this.props.futureHolidays[0].title} (${this.props.formatDate(
-                this.props.futureHolidays[0].date
-              )})`
-            : 'Loading...'}
-        </h1>
+        <DropShadowContainer>
+          <P center>The next bank holiday is on</P>
+          <Heading center>
+            {this.props.futureHolidays.length !== 0
+              ? `${this.props.formatDate(this.props.futureHolidays[0].date)} (${
+                  this.props.futureHolidays[0].title
+                })`
+              : 'Loading...'}
+          </Heading>
+        </DropShadowContainer>
       </Fragment>
     )
   }
