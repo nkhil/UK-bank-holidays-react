413dcc0e36a6e45773850c55ede2f033fc28d9d8
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-05 22:23:31 +0100

    Add dropshadow container component

---
+++ src/App/styled_components/elements/DropShadowContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DropShadowContainer = styled.div`
+  background-color: #fff;
+  box-shadow: rgba(25, 4, 69, 0.1) 0px 4px 10px;
+  padding: 20px 50px;
+  border-radius: 5px;
+  margin-bottom: 50px;
+`
+
+export default DropShadowContainer
