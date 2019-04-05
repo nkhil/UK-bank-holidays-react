7c2d5e311ae5ffb8a92bf563473676f970e0a35c
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-06 00:53:34 +0100

    Add FooterContainer component

---
+++ src/App/styled_components/elements/FooterContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const FooterContainer = styled.div`
+  padding: 20px 0;
+  margin: 50px 0 0 0;
+`
+
+export default FooterContainer
