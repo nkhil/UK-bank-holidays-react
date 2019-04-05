ae7ffd2a7ab71e538a9e2f39d6c0cbbd843119a0
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-05 22:53:34 +0100

    Add heading style to HolidayList component

--- src/App/components/HolidayList.js
+++ src/App/components/HolidayList.js
@@ -1,13 +1,14 @@
 import React, { PureComponent, Fragment } from 'react'
+import Heading from '../styled_components/elements/Heading'
 
 class HolidayList extends PureComponent {
   render() {
     return (
       <Fragment>
-        <h2>
+        <Heading h3 center>
           List of upcoming bank holidays in {new Date().getUTCFullYear()} and{' '}
           {new Date().getUTCFullYear() + 1}
-        </h2>
+        </Heading>
         <ul>
           {this.props.futureHolidays.map(holiday => {
             return this.props.createHolidayList(holiday)
