c5fae67391eb567311c06fc904473b92f650f13e
Nikhil Vijayan <nkhilv@gmail.com>
2019-04-06 23:03:08 +0100

    Modify Router to fix routing issue

--- src/App/components/Router.js
+++ src/App/components/Router.js
@@ -1,13 +1,14 @@
-import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import App from "../App";
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import App from '../App'
 
 const Router = () => (
-  <BrowserRouter basename={"/"}>
+  <BrowserRouter basename={'/'}>
     <Switch>
       <Route exact={true} path="/" component={App} />
+      <Route path="*" component={App} />
     </Switch>
   </BrowserRouter>
-);
+)
 
-export default Router;
+export default Router
